e92a766cf4c2252933583fee0068ecbf1d5d34f0
whyccup <18149106608@163.com>
2020-11-03 09:54:02 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <router-view :key="key" />
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    key() {
+      // 解决同一组件路由跳转，数据不刷新问题
+      return this.$route.name !== undefined
+        ? this.$route.name + new Date()
+        : this.$route + new Date();
+    },
+  },
+};
+</script>
